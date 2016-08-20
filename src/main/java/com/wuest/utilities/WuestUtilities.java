3c3d6be229ad2d8bad8575aec068866020266550
Brian-Wuest <redknightofdoom@yahoo.com>
2016-08-20 16:23:56 -0500

    Bumping version

--- src/main/java/com/wuest/utilities/WuestUtilities.java
+++ src/main/java/com/wuest/utilities/WuestUtilities.java
@@ -25,7 +25,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 public class WuestUtilities 
 {
 	public static final String MODID = "wuestutilities";
-	public static final String VERSION = "1.10.2.3";
+	public static final String VERSION = "1.10.2.4";
 
 	@Instance(value = WuestUtilities.MODID)
 	public static WuestUtilities instance;
