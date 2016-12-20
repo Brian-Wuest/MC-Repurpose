d8c7b1804dc09013bc4be8240f8fb1f488fb8dbb
WuestMan <redknightofdoom@yahoo.com>
2016-12-19 19:00:24 -0600

    version bump to 1.11.0.1

--- src/main/java/com/wuest/utilities/WuestUtilities.java
+++ src/main/java/com/wuest/utilities/WuestUtilities.java
@@ -25,7 +25,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 public class WuestUtilities 
 {
 	public static final String MODID = "wuestutilities";
-	public static final String VERSION = "1.11.0.0";
+	public static final String VERSION = "1.11.0.1";
 	
 	// compilation flag used for debugging purposes.
 	public static boolean isDebug = false;
