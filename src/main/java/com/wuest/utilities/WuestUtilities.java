b8ed6d5dffb54278040215a3f6ef314d01e35709
Brian-Wuest <Brian.Wuest@outlook.com>
2017-09-16 14:03:36 -0500

    version bump.

--- src/main/java/com/wuest/utilities/WuestUtilities.java
+++ src/main/java/com/wuest/utilities/WuestUtilities.java
@@ -25,7 +25,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 public class WuestUtilities 
 {
 	public static final String MODID = "wuestutilities";
-	public static final String VERSION = "1.11.2.1";
+	public static final String VERSION = "1.11.2.2";
 	
 	// compilation flag used for debugging purposes.
 	public static boolean isDebug = false;
