2e39c416c2383ddc368b6a125d29da6f79163b34
Brian Wuest <brian.wuest@outlook.com>
2019-09-15 08:04:37 -0500

    Some more block updates

--- src/main/java/com/wuest/repurpose/Blocks/BlockEnrichedFarmland.java
+++ src/main/java/com/wuest/repurpose/Blocks/BlockEnrichedFarmland.java
@@ -1,48 +1,43 @@
 package com.wuest.repurpose.Blocks;
 
 import java.util.List;
-import java.util.Random;
 
 import javax.annotation.Nullable;
 
 import com.wuest.repurpose.ModRegistry;
-import com.wuest.repurpose.Repurpose;
-import com.wuest.repurpose.Proxy.CommonProxy;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockFarmland;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FarmlandBlock;
 import net.minecraft.block.SoundType;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.Minecraft;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.ToolType;
 
-public class BlockEnrichedFarmland extends BlockFarmland
-{
-	public BlockEnrichedFarmland()
-	{
-		super();
+public class BlockEnrichedFarmland extends FarmlandBlock implements IModBlock {
+	public BlockEnrichedFarmland() {
+		super(Block.Properties.create(Material.EARTH).hardnessAndResistance(0.6F).sound(SoundType.GROUND)
+				.harvestLevel(0).harvestTool(ToolType.SHOVEL));
 
 		// Make sure that this block is always wet.
-		this.setDefaultState(this.blockState.getBaseState().withProperty(MOISTURE, Integer.valueOf(7)));
-		this.setTickRandomly(false);
-		this.setSoundType(SoundType.GROUND);
-		this.setHarvestLevel("shovel", 0);
-		this.setHardness(0.6F);
-		this.setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+		this.setDefaultState(this.stateContainer.getBaseState().with(MOISTURE, Integer.valueOf(7)));
+		
 		ModRegistry.setBlockName(this, "block_enriched_farmland");
 	}
 
@@ -50,93 +45,68 @@ public class BlockEnrichedFarmland extends BlockFarmland
 	 * Block's chance to react to a living entity falling on it.
 	 */
 	@Override
-	public void onFallenUpon(World worldIn, BlockPos pos, Entity entityIn, float fallDistance)
-	{
+	public void onFallenUpon(World worldIn, BlockPos pos, Entity entityIn, float fallDistance) {
 		// Falling on this block won't turn it to dirt.
 		entityIn.fall(fallDistance, 1.0F);
 	}
 
-	/**
-	 * Checks if this soil is fertile, typically this means that growth rates
-	 * of plants on this soil will be slightly sped up.
-	 * Only vanilla case is tilledField when it is within range of water.
-	 *
-	 * @param world The current world
-	 * @param pos Block position in world
-	 * @return True if the soil should be considered fertile.
-	 */
 	@Override
-	public boolean isFertile(World world, BlockPos pos)
-	{
-		return true;
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		return VoxelShapes.fullCube();
 	}
 
 	/**
-	 * Called when a neighboring block changes.
+	 * Update the provided state given the provided neighbor facing and neighbor
+	 * state, returning a new state. For example, fences make their connections to
+	 * the passed in state if possible, and wet concrete powder immediately returns
+	 * its solidified counterpart. Note that this method should ideally consider
+	 * only the specific face passed in.
 	 */
-	@Override
-	public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block neighborBlock, BlockPos p_189540_5_)
-	{
+	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn,
+			BlockPos currentPos, BlockPos facingPos) {
+		return stateIn;
 	}
 
 	/**
-	 * Determines if this block can support the passed in plant, allowing it to be planted and grow.
-	 * Some examples:
-	 *   Reeds check if its a reed, or if its sand/dirt/grass and adjacent to water
-	 *   Cacti checks if its a cacti, or if its sand
-	 *   Nether types check for soul sand
-	 *   Crops check for tilled soil
-	 *   Caves check if it's a solid surface
-	 *   Plains check if its grass or dirt
-	 *   Water check if its still water
+	 * Determines if this block can support the passed in plant, allowing it to be
+	 * planted and grow. Some examples: Reeds check if its a reed, or if its
+	 * sand/dirt/grass and adjacent to water Cacti checks if its a cacti, or if its
+	 * sand Nether types check for soul sand Crops check for tilled soil Caves check
+	 * if it's a solid surface Plains check if its grass or dirt Water check if its
+	 * still water
 	 *
-	 * @param state The Current state
-	 * @param world The current world
-	 * @param pos Block position in world
-	 * @param direction The direction relative to the given position the plant wants to be, typically its UP
+	 * @param state     The Current state
+	 * @param world     The current world
+	 * @param pos       Block position in world
+	 * @param direction The direction relative to the given position the plant wants
+	 *                  to be, typically its UP
 	 * @param plantable The plant that wants to check
 	 * @return True to allow the plant to be planted/stay.
 	 */
 	@Override
-	public boolean canSustainPlant(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing direction, net.minecraftforge.common.IPlantable plantable)
-	{
+	public boolean canSustainPlant(BlockState state, IBlockReader world, BlockPos pos, Direction facing,
+			net.minecraftforge.common.IPlantable plantable) {
 		// This block can sustain everything.
 		return true;
 	}
 
 	/**
-	 * Get the Item that this Block should drop when harvested.
+	 * allows items to add custom lines of information to the mouseover description
 	 */
+	@OnlyIn(Dist.CLIENT)
 	@Override
-	public Item getItemDropped(IBlockState state, Random rand, int fortune)
-	{
-		return Item.getItemFromBlock(this);
-	}
+	public void addInformation(ItemStack stack, @Nullable IBlockReader world, List<ITextComponent> tooltip,
+			ITooltipFlag advanced) {
+		super.addInformation(stack, world, tooltip, advanced);
 
-	@Override
-	public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state)
-	{
-		return new ItemStack(ModRegistry.EnrichedFarmland());
-	}
+		boolean advancedKeyDown = Screen.hasShiftDown();
 
-    /**
-     * allows items to add custom lines of information to the mouseover description
-     */
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void addInformation(ItemStack stack, @Nullable World world, List<String> tooltip, ITooltipFlag advanced)
-    {
-    	super.addInformation(stack, world, tooltip, advanced);
-    	
-    	boolean advancedKeyDown = Minecraft.getMinecraft().currentScreen.isShiftKeyDown();
-    	
-    	if (!advancedKeyDown)
-    	{
-    		tooltip.add("Hold" + TextFormatting.BLUE + " Shift " + TextFormatting.GRAY +  "for advanced information.");
-    	}
-    	else
-    	{
-    		tooltip.add("This hardy soil always remains hydrated and tilled, no matter the climate!");
-    	}
-    }
+		if (!advancedKeyDown) {
+			tooltip.add(new StringTextComponent(
+					"Hold" + TextFormatting.BLUE + " Shift " + TextFormatting.GRAY + "for advanced information."));
+		} else {
+			tooltip.add(new StringTextComponent(
+					"This hardy soil always remains hydrated and tilled, no matter the climate!"));
+		}
+	}
 }
--- src/main/java/com/wuest/repurpose/Blocks/BlockMiniRedstone.java
+++ src/main/java/com/wuest/repurpose/Blocks/BlockMiniRedstone.java
@@ -1,120 +1,62 @@
 package com.wuest.repurpose.Blocks;
 
 import com.wuest.repurpose.ModRegistry;
-import com.wuest.repurpose.Repurpose;
-import com.wuest.repurpose.Proxy.CommonProxy;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockCompressedPowered;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.RedstoneBlock;
 import net.minecraft.block.SoundType;
-import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;;
 
-public class BlockMiniRedstone extends BlockCompressedPowered
-{
-	protected static final AxisAlignedBB bounds = new AxisAlignedBB(
-			0.25D, 
-			0.0D, 
-			0.25D, 
-			0.75D, 
-			0.50D, 
-			0.75D);
+public class BlockMiniRedstone extends RedstoneBlock implements IModBlock {
+	protected static final AxisAlignedBB bounds = new AxisAlignedBB(0.25D, 0.0D, 0.25D, 0.75D, 0.50D, 0.75D);
+	protected static final VoxelShape shape = VoxelShapes.create(BlockMiniRedstone.bounds);
 
 	/**
 	 * Initializes a new instance of the BlockMiniRedstone.
 	 */
-	public BlockMiniRedstone()
-	{
-		super(Material.IRON, MapColor.TNT);
-		this.setCreativeTab(CreativeTabs.REDSTONE);
-		ModRegistry.setBlockName(this, "block_mini_redstone");
-		this.setHarvestLevel(null, 0);
-		this.setHardness(.5f);
-		this.setResistance(10.0f);
-		this.setSoundType(SoundType.METAL);
-	}
-		
-	@Override
-    public int getWeakPower(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side)
-    {
-        return 8;
-    }
-	
-	@Override
-	public boolean isFullCube(IBlockState state)
-	{
-		return false;
-	}
-	
-    /**
-     * Used to determine ambient occlusion and culling when rebuilding chunks for render
-     */
-    @Override
-	public boolean isOpaqueCube(IBlockState state)
-    {
-        return false;
-    }
-	
-	@Override
-    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
-    {
-    	return BlockMiniRedstone.bounds;
-    }
+	public BlockMiniRedstone() {
+		super(Block.Properties.create(Material.IRON, MaterialColor.TNT).harvestLevel(0).harvestTool(null)
+				.hardnessAndResistance(.5f, 10.0f).sound(SoundType.METAL));
 
-	@Override
-    public AxisAlignedBB getSelectedBoundingBox(IBlockState blockState, World worldIn, BlockPos pos)
-    {
-        return BlockMiniRedstone.bounds;
-    }
-	
+		ModRegistry.setBlockName(this, "block_mini_redstone");
+	}
+
+	public ItemGroup getItemGroup() {
+		return ItemGroup.REDSTONE;
+	}
+
 	/**
-     * Determines if the player can harvest this block, obtaining it's drops when the block is destroyed.
-     *
-     * @param player The player damaging the block, may be null
-     * @param meta The block's current metadata
-     * @return True to spawn the drops
-     */
-    @Override
-	public boolean canHarvestBlock(IBlockAccess world, BlockPos pos, EntityPlayer player)
-    {
-        return true;
-    }
-	
+	 * @deprecated call via
+	 *             {@link IBlockState#getWeakPower(IBlockAccess,BlockPos,EnumFacing)}
+	 *             whenever possible. Implementing/overriding is fine.
+	 */
+	public int getWeakPower(BlockState blockState, IBlockReader blockAccess, BlockPos pos, Direction side) {
+		return 15;
+	}
+
+	@Deprecated
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		return BlockMiniRedstone.shape;
+	}
+
 	/**
-	 * Determines if a torch can be placed on the top surface of this block.
-	 * Useful for creating your own block that torches can be on, such as fences.
-	 *
-	 * @param state The current state
-	 * @param world The current world
-	 * @param pos Block position in world
-	 * @return True to allow the torch to be placed
+	 * Gets the render layer this block will render on. SOLID for solid blocks,
+	 * CUTOUT or CUTOUT_MIPPED for on-off transparency (glass, reeds), TRANSLUCENT
+	 * for fully blended transparency (stained glass)
 	 */
 	@Override
-	public boolean canPlaceTorchOnTop(IBlockState state, IBlockAccess world, BlockPos pos)
-	{
-		return false;
+	public BlockRenderLayer getRenderLayer() {
+		return BlockRenderLayer.CUTOUT;
 	}
-	
-    @SideOnly(Side.CLIENT)
-    @Override
-    public BlockRenderLayer getBlockLayer()
-    {
-        return BlockRenderLayer.CUTOUT;
-    }
 }
